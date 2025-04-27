cff324c2e53309d987272c808c3305b44670af4c
GaryChau1 <s4005858@student.rmit.edu.au>
2025-04-27 16:06:37 +1000

    added getName() which stores url employee value

--- src/main/java/employee/service/EmployeeServiceApplication.java
+++ src/main/java/employee/service/EmployeeServiceApplication.java
@@ -2,6 +2,8 @@ package employee.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,14 @@ public class EmployeeServiceApplication {
 
     @RequestMapping("/")
     String home(){
-        return "hello world";
+        return "hi";
     }
+
+    @GetMapping("/employees/{name}")
+    public String getName(@PathVariable String name){
+        return "Name is: " + name;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
